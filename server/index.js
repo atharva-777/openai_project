9ae0db942011694a642dbf38a9b3daa811213413
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-03-30 23:55:31 +0530

    backend setup done

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit:'30mb',extended:true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors())
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log(`Listening to port : ${PORT}`)
+})
